3000cddd7b718a5b36621622984660fa5f817e4f
bear <593788753@qq.com>
2019-04-24 21:33:08 +0800

    first

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+* @Author: bear
+* @Date:   2019-04-24 21:01:28
+* @Last Modified by:   bear
+* @Last Modified time: 2019-04-24 21:06:09
+*/
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+
+}
