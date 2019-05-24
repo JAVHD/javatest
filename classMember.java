187d826a86c5d195c48dc2b88dbe7deb23375f4c
bear <593788753@qq.com>
2019-05-24 14:00:50 +0800

    无法从静态上下文中引用非静态 方法 getString()

--- classMember.java
+++ classMember.java
@@ -2,19 +2,37 @@
 * @Author: bear
 * @Date:   2019-05-24 11:48:34
 * @Last Modified by:   bear
-* @Last Modified time: 2019-05-24 11:53:43
+* @Last Modified time: 2019-05-24 14:00:02
 */
 
 public class classMember {
 
-	private static String name = "bear";
+	// private static String name = "bear";
 
-	public static String getString() {
+	// public static String getString() {
+	// 	return name;
+	// }
+
+	// classMember.java:17: 错误: 无法从静态上下文中引用非静态 方法 getString()
+	// 	System.out.println(getString());
+	// 	                   ^
+	// 1 个错误
+	// beardeMacBook-Pro:javatest bear$ javac classMember.java
+	// classMember.java:13: 错误: 无法从静态上下文中引用非静态 变量 name
+	// 		return name;
+
+	private String name = "bear";
+
+	public String getString() {
 		return name;
 	}
 
+
 	public static void main(String[] args) {
-		System.out.println(getString());
+		//System.out.println(getString());
+
+		classMember cm = new classMember();
+		System.out.println(cm.getString());
 	}
 
 }
--- getDeclaredFieldsFunc.java
+++ getDeclaredFieldsFunc.java
@@ -2,7 +2,7 @@
 * @Author: bear
 * @Date:   2019-05-21 22:18:55
 * @Last Modified by:   bear
-* @Last Modified time: 2019-05-21 22:43:57
+* @Last Modified time: 2019-05-21 22:32:26
 */
 
 import java.lang.reflect.Field;
@@ -22,10 +22,8 @@ public class getDeclaredFieldsFunc {
 	public static void main(String[] args) {
 
 		Example example = new Example();
-
-		Class exampleClass = example.getClass();
-
-		Field[] declaredFields = exampleClass.getDeclaredFields();
+		
+		example.getClass()
 
 	}
 }
