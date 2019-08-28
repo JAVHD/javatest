6b40a428b060d32266286db6f3b53f1fdac3b47b
bear <593788753@qq.com>
2019-08-28 15:34:04 +0800

    Map

---
+++ mapType.java
@@ -0,0 +1,25 @@
+/*
+* @Author: bear
+* @Date:   2019-08-28 15:08:53
+* @Last Modified by:   bear
+* @Last Modified time: 2019-08-28 15:20:13
+*/
+
+import java.util.Map;
+import java.util.HashMap;
+
+
+
+public class mapType {
+
+	public static void main(String[] args) {
+
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("name", "bear");
+		map.put("age", "18");
+
+		System.out.println(map);
+
+	}
+
+}
