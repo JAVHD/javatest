4ebae8e9cb3c5791692a2054ada0df4cf6c163f2
bear <593788753@qq.com>
2019-09-03 11:27:57 +0800

    a.longvalue()

--- ListType.java
+++ ListType.java
@@ -2,13 +2,15 @@
 * @Author: bear
 * @Date:   2019-09-02 22:35:37
 * @Last Modified by:   bear
-* @Last Modified time: 2019-09-02 22:36:38
+* @Last Modified time: 2019-09-03 11:09:37
 */
 
 public class ListType {
 	public static void main(String[] args) {
 
 		List<Object> list = new ArrayList<Object>();
+
+		list.add()
 
 	}
 }
--- timeToDate.java
+++ timeToDate.java
@@ -2,7 +2,7 @@
 * @Author: bear
 * @Date:   2019-09-03 09:59:17
 * @Last Modified by:   bear
-* @Last Modified time: 2019-09-03 10:13:09
+* @Last Modified time: 2019-09-03 11:23:26
 */
 
 import java.util.Date;
@@ -12,10 +12,12 @@ public class timeToDate {
 
 	public static void main(String[] args) {
 		//time = "1555882875";
-		long time = 1555882875;
+		//long time = 1555841737;
+		Integer time = 1555841737;
 
-		String date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(time * 1000));
+		String date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date(time.longValue() * 1000));
 
+		System.out.println("new Date: " + new Date(time * 1000));
 		System.out.println("time " + time);
 		System.out.println("date " + date);
 
