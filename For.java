6adea4232981dd9b642aeb5c76acc40381b95401
bear <593788753@qq.com>
2019-10-25 11:55:25 +0800

    for(each)

--- For.java
+++ For.java
@@ -2,15 +2,34 @@
 * @Author: bear
 * @Date:   2019-10-24 23:40:37
 * @Last Modified by:   bear
-* @Last Modified time: 2019-10-24 23:48:32
+* @Last Modified time: 2019-10-25 11:49:07
 */
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class For {
 	public static void main(String[] args) {
 		// for ( : ) {
 
 		// }
-
 		//fo
+		//ArrayList<String> arrayList = new ArrayList<>(Arrays.asList("cat", "dog", "bear", "panda"));
+
+		ArrayList<String> arrayList = new ArrayList<>();
+
+		arrayList.add("bear");
+		arrayList.add("dog");
+		arrayList.add("panda");
+
+		//String array[] = {"cat", "dog", "bear", "panda"};
+		//int array[] = {99, 100, 101, 102};
+
+
+		for (String a : arrayList
+		    ) {
+			System.out.println(a);
+		}
+
 	}
 }
