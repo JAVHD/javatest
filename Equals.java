d635670efd6075532d5b593595230af248bb8d6d
bear <593788753@qq.com>
2019-11-05 17:08:56 +0800

    int hashCode()

---
+++ Equals.java
@@ -0,0 +1,12 @@
+/*
+* @Author: bear
+* @Date:   2019-11-05 15:26:08
+* @Last Modified by:   bear
+* @Last Modified time: 2019-11-05 15:28:55
+*/
+
+public class Equals {
+	public static void main(String[] args) {
+		//object类的equals()方法的比较规则为：如果两个对象的类型一致，并且内容一致，则返回true
+	}
+}
---
+++ HashCode.java
@@ -0,0 +1,15 @@
+/*
+* @Author: bear
+* @Date:   2019-11-05 15:12:31
+* @Last Modified by:   bear
+* @Last Modified time: 2019-11-05 15:13:23
+*/
+
+public class HashCode {
+	public static void main(String[] args) {
+		String name = "bear";
+		int hashCode = name.hashCode();
+		//hash(hashCode);
+		System.out.println(hashCode);
+	}
+}
