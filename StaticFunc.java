a99aadae1f1b1d6e1737528a51b39215b0fbc0ec
bear <593788753@qq.com>
2019-11-07 17:51:59 +0800

    无法从静态上下文中引用非静态 方法

---
+++ StaticFunc.java
@@ -0,0 +1,16 @@
+/*
+* @Author: bear
+* @Date:   2019-11-07 17:39:01
+* @Last Modified by:   bear
+* @Last Modified time: 2019-11-07 17:39:52
+*/
+
+public class StaticFunc {
+	public static void main(String[] args) {
+		System.out.println(sayHello());
+	}
+
+	public String sayHello() {
+		return "hello";
+	}
+}
