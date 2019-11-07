a82ab4cf737653323a71a3fffac56963804e04f5
bear <593788753@qq.com>
2019-11-07 13:42:12 +0800

    runnable多线程

--- HashCode.java
+++ HashCode.java
@@ -2,14 +2,16 @@
 * @Author: bear
 * @Date:   2019-11-05 15:12:31
 * @Last Modified by:   bear
-* @Last Modified time: 2019-11-05 15:13:23
+* @Last Modified time: 2019-11-06 10:29:52
 */
 
 public class HashCode {
 	public static void main(String[] args) {
 		String name = "bear";
 		int hashCode = name.hashCode();
-		//hash(hashCode);
+		hash(hashCode);
 		System.out.println(hashCode);
 	}
+
+
 }
