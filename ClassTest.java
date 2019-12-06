e27a3a8a6bbdab7a0924a437fadedfe5f3d26616
bear <593788753@qq.com>
2019-12-07 00:23:32 +0800

    int.class getName()

---
+++ ClassTest.java
@@ -0,0 +1,14 @@
+/*
+* @Author: bear
+* @Date:   2019-12-07 00:06:39
+* @Last Modified by:   bear
+* @Last Modified time: 2019-12-07 00:21:46
+*/
+
+public class ClassTest {
+	public static void main(String[] args) {
+		Class<Integer> cls =  int.class;
+
+		System.out.println(cls.getName());
+	}
+}
