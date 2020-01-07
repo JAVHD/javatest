c3406a952aa8265d5655157f8987e40294acd0f1
bear <593788753@qq.com>
2020-01-07 15:01:01 +0800

    java LocalDateTime

---
+++ LocalDateTimeTest.java
@@ -0,0 +1,17 @@
+/*
+* @Author: bear
+* @Date:   2020-01-07 14:52:36
+* @Last Modified by:   bear
+* @Last Modified time: 2020-01-07 14:54:38
+*/
+
+import java.time.LocalDateTime;
+
+public class LocalDateTimeTest {
+	public static void main(String[] args) {
+
+		LocalDateTime ldt = LocalDateTime.now();
+
+		System.out.println(ldt);
+	}
+}
