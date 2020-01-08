fbf82f34621fc97b0fb0e9df77f744af6f327153
bear <593788753@qq.com>
2020-01-08 11:47:35 +0800

    BaseDataType

---
+++ BaseDataType.java
@@ -0,0 +1,22 @@
+/*
+* @Author: bear
+* @Date:   2020-01-08 11:36:43
+* @Last Modified by:   bear
+* @Last Modified time: 2020-01-08 11:45:39
+* @Description 八大基本数据类型
+*/
+
+public class BaseDataType {
+	public static void main(String[] args) {
+
+		Short a; //->Numner->i:Comparable<T>
+		Integer b; //->Numner->i:Comparable<T>
+		Long c; //->Numner->i:Comparable<T>
+		Float d; //->Numner->i:Comparable<T>
+		Double e; //->Numner->i:Comparable<T>
+		Byte f; //->Numner->i:Comparable<T>
+		Character g; //->i:java.io.Serializable, Comparable<Character>
+		Boolean h; //->i:java.io.Serializable, Comparable<Boolean>
+
+	}
+}
