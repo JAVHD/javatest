16338619b6cd7b65c1f36dad6f779e06ce3fc9bb
bear <593788753@qq.com>
2020-04-18 10:46:21 +0800

    == 值和引用都要相等；equals 值（内容）相等

--- equalsFunc.java
+++ equalsFunc.java
@@ -12,11 +12,29 @@ public class equalsFunc {
 		String s1 = "123";
 		String s2 = "123";
 
+		String s3 = new String("123");
+		String s4 = new String("123");
+
+		Integer age1 = 18;
+		Integer age2 = 18;
+
+		Integer age3 = new Integer(18);
+		Integer age4 = new Integer(18);
+
 		obj o1 = new obj();
 		obj o2 = new obj();
 
-		System.out.println(s1.equals(s2));
-		System.out.println(o1.equals(o2));
+		System.out.println(s1 == s2); //true
+		System.out.println(s3 == s4); //false
+		System.out.println(s3.equals(s4)); //true
+
+		System.out.println(age1 == age2); //true
+		System.out.println(age3 == age4); //false
+		System.out.println(o1 == o2); //false
+		System.out.println(s1.equals(s2)); //true
+		System.out.println(o1.equals(o2)); //false
+
+
 
 	}
 
