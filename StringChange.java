720801490f74a46217674335b7fda0f569b02986
bear <593788753@qq.com>
2020-04-18 15:48:41 +0800

    String

---
+++ StringChange.java
@@ -0,0 +1,13 @@
+
+public class StringChange {
+    public static void main(String[] args) {
+        String name = "hello world";
+
+        String brand = "apple";
+
+        brand = name;
+
+        System.out.println(brand); //hello world
+
+    }
+}
