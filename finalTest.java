6bce6ba2e7c3b381e287fe52ca2b174f7eaec2f5
bear <593788753@qq.com>
2020-04-19 23:44:52 +0800

    静态变量和静态常量属于类，不属于对象，因此它们不能被覆盖

--- finalTest.java
+++ finalTest.java
@@ -2,19 +2,27 @@
 public class finalTest {
     public static void main(String[] args) {
         System.out.println(ClassB.getAge());
+        System.out.println(ClassB.getInch());
+
     }
 
     public static class ClassA {
         public static final int age = 18; //final不可被重写
+        public static int inch = 13; //静态变量和静态常量属于类，不属于对象，因此它们不能被覆盖
 
         public static int getAge() {
             return age;
+        }
+
+        public static int getInch() {
+            return inch;
         }
 
     }
 
     public static class ClassB extends ClassA {
         public static final int age = 30;
+        public static int inch = 15;
 
 //        public static test() {
 //            System.out.println("ClassB.test()");
